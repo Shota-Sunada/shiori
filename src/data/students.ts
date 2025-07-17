128a20547712c3676a69adf259a3e02d22ce2c5c
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-07-17 21:00:53 +0900

    キーを作成

--- src/data/students.ts
+++ src/data/students.ts
@@ -1,8 +1,27 @@
 export type student = {
-  surname: string
-  forename: string
-  class: number
-  number: number
-  day1id: number
-  day3id: number
+  surname: string;
+  forename: string;
+  class: 1 | 2 | 3 | 4 | 5 | 6 | 7;
+  number: number;
+  day1id:
+    | "yrp_nifco"
+    | "yrp_yamashin"
+    | "yrp_air"
+    | "yrp_vtech"
+    | "ntt_labo_i"
+    | "ntt_labo_b"
+    | "kayakku"
+    | "iaxa"
+    | "astro"
+    | "arda"
+    | "urth_jip"
+    | "micro"
+    | "air";
+  day3id:
+    | "okutama"
+    | "yokosuka"
+    | "hakone"
+    | "kamakura"
+    | "hakkeijima"
+    | "yokohama";
 };
