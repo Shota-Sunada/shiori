76d0a73ef48b69b83034dd0b6f92d0b217d6824b
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-07-20 11:15:15 +0900

    暗号化実装

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,36 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigpaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigpaths(), tailwindcss()]
+  plugins: [
+    react(),
+    tsconfigpaths(),
+    tailwindcss(),
+    obfuscatorPlugin({
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: true,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        // domainLock: ['.shudo-physics.com'],
+        log: false,
+        renameGlobals: false,
+        rotateStringArray: true,
+        sourceMap: false,
+        selfDefending: true,
+        stringArray: true,
+        stringArrayEncoding: ['base64'],
+        stringArrayThreshold: 0.75,
+        transformObjectKeys: true,
+        unicodeEscapeSequence: false
+      }
+    })
+  ]
 });
