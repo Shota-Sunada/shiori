3e1ee297c332d7f246a9bab1a2c56e8c33128168
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-07-26 13:29:25 +0900

    firebase微調整

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,9 +8,11 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
+console.log('Firebase config:', firebaseConfig);
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
