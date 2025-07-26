eab713923ce7f40c469aa2f8bb0a6163de604f78
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-07-26 23:12:11 +0900

    Button修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
 interface Props {
   text: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
 const Button = (props: Props) => {
   return (
-    <button className="mobiry-button flex flex-row items-center justify-center relative" onClick={props.onClick}>
+    <div className="mobiry-button flex flex-row items-center justify-center relative" onClick={props.onClick}>
       <p className="font-medium">{props.text}</p>
-    </button>
+    </div>
   );
 };
 
