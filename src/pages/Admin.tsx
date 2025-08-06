23388449d15ea933477e0e63ca55ebf41c0cdbf6
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-08-06 23:13:22 +0900

    微調整

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -348,12 +348,15 @@ const Admin: React.FC = () => {
   };
   return (
     <div>
-      <button className="m-[10px]" disabled={modalMode !== null}>
-        <Button text="新規追加" onClick={handleAddRow} arrow={false} />
-      </button>
-      <button className="m-[10px]" disabled={modalMode !== null}>
-        <Button text="JSONでまとめて追加" onClick={handleAddJSONData} arrow={false} />
-      </button>
+      <p className='m-[10px] text-4xl'>{"管理画面"}</p>
+      <div className='flex flex-row'>
+        <button className="" disabled={modalMode !== null}>
+          <Button text="新規追加" onClick={handleAddRow} arrow={false} />
+        </button>
+        <button className="" disabled={modalMode !== null}>
+          <Button text="JSONでまとめて追加" onClick={handleAddJSONData} arrow={false} />
+        </button>
+      </div>
       <input
         className="m-[10px]"
         ref={inputRef}
