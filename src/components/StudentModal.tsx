19b7482c94d380d43261792c56d7a7691b245ba5
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-08-07 10:12:42 +0900

    囲う

--- src/components/StudentModal.tsx
+++ src/components/StudentModal.tsx
@@ -61,17 +61,17 @@ const StudentModal = ({
       <div className="border bg-white text-black rounded-xl px-[5dvw] py-[5dvh] min-w-[350px] max-h-[90dvh] overflow-y-auto">
         <h2 className="mb-4">{mode === 'add' ? '生徒データ新規追加' : '生徒データ編集'}</h2>
         <div className="grid grid-cols-2 gap-2">
-          <label>学籍番号</label>
+          <label>{'学籍番号'}</label>
           <input name="gakuseki" value={form.gakuseki} onChange={handleChange} required />
-          <label>姓</label>
+          <label>{'姓'}</label>
           <input name="surname" value={form.surname} onChange={handleChange} required />
-          <label>名</label>
+          <label>{'名'}</label>
           <input name="forename" value={form.forename} onChange={handleChange} required />
-          <label>組</label>
+          <label>{'組'}</label>
           <input name="class" type="number" min={1} max={7} value={form.class} onChange={handleChange} required />
-          <label>番号</label>
+          <label>{'番号'}</label>
           <input name="number" type="number" min={1} max={41} value={form.number} onChange={handleChange} required />
-          <label>一日目研修先</label>
+          <label>{'一日目研修先'}</label>
           <select name="day1id" value={form.day1id} required onChange={handleChange}>
             {day1idOptions.map((opt) => (
               <option key={opt} value={opt}>
@@ -79,7 +79,7 @@ const StudentModal = ({
               </option>
             ))}
           </select>
-          <label>三日目研修先</label>
+          <label>{'三日目研修先'}</label>
           <select name="day3id" value={form.day3id} required onChange={handleChange}>
             {day3idOptions.map((opt) => (
               <option key={opt} value={opt}>
@@ -87,13 +87,13 @@ const StudentModal = ({
               </option>
             ))}
           </select>
-          <label>一日目バス</label>
+          <label>{'一日目バス'}</label>
           <input name="day1bus" value={form.day1bus} required onChange={handleChange} />
-          <label>三日目バス</label>
+          <label>{'三日目バス'}</label>
           <input name="day3bus" value={form.day3bus} required onChange={handleChange} />
-          <label>東京ドームホテル 号室</label>
+          <label>{'東京ドームホテル 号室'}</label>
           <input name="room_tokyo" value={form.room_tokyo} required onChange={handleChange} />
-          <label>静岡 ホテル 号室</label>
+          <label>{'フジプレミアムリゾート 号室'}</label>
           <input name="room_shizuoka" value={form.room_shizuoka} required onChange={handleChange} />
         </div>
         <div className="flex flex-row items-center justify-center mt-4">
