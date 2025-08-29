accae02678207f3ce817e3c936e02cf44e30c911
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-08-29 09:59:36 +0900

    py側も適用

--- sha256.py
+++ sha256.py
@@ -1,8 +1,8 @@
 import hashlib
 import os
 
-if os.path.isfile("sha256list.txt"):
-    with open("sha256list.txt", "r", encoding="utf-8") as file:
+if os.path.isfile("sha256input.txt"):
+    with open("sha256input.txt", "r", encoding="utf-8") as file:
         line = file.readline()
         while line:
             text = line.rstrip("\n")
