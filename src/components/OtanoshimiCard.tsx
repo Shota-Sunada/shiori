6dd0d8b5b96b7f2a646c1e8dd2e7fdae9cda32b2
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-08-29 13:57:57 +0900

    修正

--- src/components/OtanoshimiCard.tsx
+++ src/components/OtanoshimiCard.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const OtanoshimiCard = (props: Props) => {
   return (
-    <div className="px-[1dvw] py-[2dvh] min-w-[8dvw] w-full min-h-[10dvh] h-full rounded-xl text-white text-center bg-gray-400 border-2 border-gray-500 flex items-center justify-center">
+    <div className="text-[120%] px-[0.5dvw] py-[1dvh] min-w-[7dvw] w-full min-h-[10dvh] h-full rounded-xl text-white text-center bg-gray-400 border-2 border-gray-500 flex items-center justify-center">
       <p>{props.name}</p>
     </div>
   );
