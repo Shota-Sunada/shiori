2f38a7c9068a9e378330e4463b06a67145da1e75
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-08-30 21:30:23 +0900

    %調整

--- src/components/StudentCardContent.tsx
+++ src/components/StudentCardContent.tsx
@@ -8,8 +8,8 @@ interface StudentCardContentProps {
 const StudentCardContent: React.FC<StudentCardContentProps> = ({ student }) => {
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="basis-[8%]">{`${student.class}組`}</div>
-      <div className="basis-[8%]">{`${student.number}番`}</div>
+      <div className="basis-[10%]">{`${student.class}組`}</div>
+      <div className="basis-[10%]">{`${student.number}番`}</div>
       <div className="basis-[35%] font-bold pl-3">{`${student.surname} ${student.forename}`}</div>
       <div className="basis-[45%] text-[#555] text-sm">{`${student.surname_kana} ${student.forename_kana}`}</div>
     </div>
