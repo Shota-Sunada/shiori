1a7b03a33520306c56af85230c89a14df920ab87
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-09-01 21:18:25 +0900

    Revert "OneSignal実装"
    This reverts commit 09bb2a52cb52202b5c71d31a9061caf0170bc4c5.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,4 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import OneSignal from 'react-onesignal';
-
-const runApp = async () => {
-  await OneSignal.init({
-    appId: '130fb1a3-f9af-4531-a73f-345da5fc6120',
-    safari_web_id: 'web.onesignal.auto.212e621b-efc2-4b2a-9d36-9f4cd158ecec',
-    allowLocalhostAsSecureOrigin: true
-  });
-  OneSignal.Slidedown.promptPush();
-};
-
-runApp();
 
 createRoot(document.getElementById('root')!).render(<App />);
