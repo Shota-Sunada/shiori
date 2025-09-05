11370bf62f9ce8900c63afe6888295bcebef2eee
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-09-06 01:47:18 +0900

    00:00

--- src/pages/Call.tsx
+++ src/pages/Call.tsx
@@ -124,6 +124,10 @@ const Call = () => {
 
   const formatTime = (totalSeconds: number) => {
     totalSeconds -= 20;
+        if (totalSeconds <= 0) {
+      return "00:00";
+    }
+    
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
