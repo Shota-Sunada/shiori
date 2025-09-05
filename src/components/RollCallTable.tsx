2312bc0dff8d2a4c48dccac0473ddcf5d1f5a15a
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-09-05 23:19:54 +0900

    表改善

--- src/components/RollCallTable.tsx
+++ src/components/RollCallTable.tsx
@@ -1,3 +1,4 @@
+import { FaArrowRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 interface RollCall {
@@ -16,33 +17,35 @@ interface RollCallTableProps {
 const RollCallTable = ({ rollCalls }: RollCallTableProps) => {
   return (
     <div className="overflow-x-auto">
-      <table className="min-w-full bg-white">
+      <table className="min-w-full max-w-[95dvw] bg-white">
         <thead>
           <tr>
-            <th className="py-2 px-4 border-b">{'ステータス'}</th>
-            <th className="py-2 px-4 border-b">{'開始日時'}</th>
-            <th className="py-2 px-4 border-b">{'出席状況'}</th>
-            <th className="py-2 px-4 border-b">{'開始した先生'}</th>
-            <th className="py-2 px-4 border-b">{'詳細'}</th>
+            <th className="py-1 px-2 border-b">{'状態'}</th>
+            <th className="py-1 px-2 border-b">{'開始日時'}</th>
+            <th className="py-1 px-2 border-b">{'状況'}</th>
+            <th className="py-1 px-2 border-b">{'先生'}</th>
+            <th className="py-1 px-2 border-b">{'詳細'}</th>
           </tr>
         </thead>
         <tbody>
           {rollCalls.map((rollCall) => (
             <tr key={rollCall.id} className={`${!rollCall.is_active ? 'bg-gray-200' : ''}`}>
-              <td className="py-2 px-4 border-b text-center">
-                {rollCall.is_active ? <span className="bg-green-500 text-white py-1 px-2 rounded">{'発動中'}</span> : <span className="bg-gray-500 text-white py-1 px-2 rounded">{'終了'}</span>}
+              <td className="py-0.5 px-2 border-b text-center min-w-18">
+                {rollCall.is_active ? <span className="bg-green-500 text-white py-1 px-1 rounded">{'発動中'}</span> : <span className="bg-gray-500 text-white py-1 px-1 rounded">{'終了'}</span>}
               </td>
-              <td className="py-2 px-4 border-b text-center">{new Date(rollCall.created_at).toLocaleString()}</td>
-              <td className="py-2 px-4 border-b text-center">
+              <td className="py-0.5 px-2 border-b text-center">{new Date(rollCall.created_at).toLocaleString()}</td>
+              <td className="py-0.5 px-2 border-b text-center">
                 {rollCall.checked_in_students}
                 {' / '}
                 {rollCall.total_students}
               </td>
-              <td className="py-2 px-4 border-b text-center">{rollCall.teacher_id}</td>
-              <td className="py-2 px-4 border-b text-center">
-                <Link to={`/teacher/call?id=${rollCall.id}`} className="text-white bg-blue-500 rounded hover:underline p-2">
-                  {'詳細'}
-                </Link>
+              <td className="py-0.5 px-2 border-b text-center">{rollCall.teacher_id}</td>
+              <td className="py-0.5 px-3 border-b text-center">
+                <div className="flex items-center justify-center">
+                  <Link to={`/teacher/call?id=${rollCall.id}`}>
+                    <FaArrowRight className='cursor-pointer bg-[#219ace30] rounded-2xl p-1.5' size={'30px'} color="#219bce" />
+                  </Link>
+                </div>
               </td>
             </tr>
           ))}
