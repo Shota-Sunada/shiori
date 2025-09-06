75adebd90e5df3907d96f45fe2ebf481d6e9fd7d
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-09-06 18:28:38 +0900

    色指定対応

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,15 +6,17 @@ interface Props {
   arrow?: boolean;
   onClick?: React.MouseEventHandler<HTMLDivElement>;
   link?: string;
+  color?: string;
+  hoverColor?: string;
 }
 
 const Button = (props: Props) => {
   const innerButton = () => (
-    <div className="mobiry-button flex flex-row items-center justify-center relative">
+    <div className={`mobiry-button flex flex-row items-center justify-center relative bg-[${props.color ? props.color : "#219bce"}] hover:bg-[${props.hoverColor ? props.hoverColor : "#008fc0"}]`}>
       <p className="font-medium">{props.text}</p>
       {props.arrow ? (
         <div className="absolute right-[6px] bg-white rounded-[50%] min-w-[28px] min-h-[28px] flex items-center justify-center">
-          <FaArrowRight size={'15px'} color="#219bce" />
+          <FaArrowRight size={'15px'} color={props.color ? props.color : '#219bce'} />
         </div>
       ) : (
         <></>
