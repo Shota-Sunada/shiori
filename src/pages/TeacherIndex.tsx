f48d9428c630a4a9a5e07fd2cbcbbcf4e661111a
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-09-07 20:38:45 +0900

    スペースを追加

--- src/pages/TeacherIndex.tsx
+++ src/pages/TeacherIndex.tsx
@@ -85,6 +85,7 @@ const TeacherIndex = () => {
                 {teacherData ? (
                   <>
                     {teacherData?.surname}
+                    {" "}
                     {teacherData?.forename}
                   </>
                 ) : (
