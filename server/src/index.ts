7d4758c5574e1051ade4996fca874858a1f5f33d
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-09-09 16:04:12 +0900

    500に変更

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 // Rate limiter for auth routes
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50, // Limit each IP to 50 requests per windowMs
+  max: 500, // Limit each IP to 500 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false // Disable the `X-RateLimit-*` headers
 });
