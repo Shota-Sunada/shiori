61ec1be7ba28cafe5ac52993ab3b109d77df73c6
Shota-Sunada <shota.sunada.dev@gmail.com>
2026-02-25 00:36:46 +0900

    ローカル化

--- src/config/serverEndpoint.ts
+++ src/config/serverEndpoint.ts
@@ -1,2 +1,3 @@
 // サーバーエンドポイントは常に固定
-export const SERVER_ENDPOINT = 'https://api.shiori.shudo-physics.com';
+// export const SERVER_ENDPOINT = 'https://api.shiori.shudo-physics.com';
+export const SERVER_ENDPOINT = 'http://localhost:8080';
